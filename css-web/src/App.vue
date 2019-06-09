8a45fb686cd6937ba3558efcaa62587eedbb4f34
JJH19 <jiangjiaheng1994@qq.com>
2019-06-09 19:10:18 +0800

    css

---
+++ css-web/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <my-header></my-header>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import MyHeader from './layout/MyHeader.vue';
+
+export default {
+  name: 'app',
+  components: {
+    MyHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ css-web/src/layout/MyHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class>
+    <el-menu class="el-menu-demo" mode="horizontal"
+    @select="handleSelect" :default-active="activeIndex">
+        <el-menu-item index="1">Home</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      activeIndex: '1',
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+      this.$router.push('about');
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
